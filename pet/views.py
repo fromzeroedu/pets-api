e9183ce9f651f2d4ec3201e58da842bf333ad205
Jorge Escobar <jorge@fromzero.io>
2016-12-26 16:53:32 -0500

    back at 2.3

--- pet/views.py
+++ pet/views.py
@@ -4,4 +4,9 @@ from pet.api import PetAPI
 
 pet_app = Blueprint('pet_app', __name__)
 
-pet_app.add_url_rule('/pets/', view_func=PetAPI.as_view('pets'))
+pet_view = PetAPI.as_view('pet_api')
+pet_app.add_url_rule('/pets/', defaults={'pet_id': None},
+                 view_func=pet_view, methods=['GET',])
+pet_app.add_url_rule('/pets/', view_func=pet_view, methods=['POST',])
+pet_app.add_url_rule('/pets/<int:pet_id>', view_func=pet_view,
+                 methods=['GET', 'PUT', 'DELETE',])
